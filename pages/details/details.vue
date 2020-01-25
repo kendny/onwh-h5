742115c49380c5a042fe3c51b2cf757104b02e09
解伟 <xiewei@jub.ai>
2020-01-25 08:29:44 +0800

    初版

---
+++ pages/details/details.vue
@@ -0,0 +1,122 @@
+<template>
+	<view class="detail-wrap">
+		<Info :detail="details"></Info>
+		<view class="detail-head">
+			<tabs ref="tab" :tabData="tabList" :defaultIndex="current" @tabClick='tabClick'></tabs>
+		</view>
+		<swiper class="swiper" :current="current" @change="tabClick" :style="{height: '100vh'}">
+			<swiper-item>
+				<view class="item-wrap">
+					<view class="item-wuzi flex-between" v-for="(child,idx) in details.details" :key="idx">
+						<text>{{child.name}}</text>
+						<text>{{child.amount}} / {{child.unit}}</text>
+					</view>
+				</view>
+				<!-- <Info :detail="details"></Info> -->
+			</swiper-item>
+			<swiper-item>
+				<view class="item-wrap">
+					<view class="item-wuzi flex-between" v-for="(child,idx) in details.details" :key="idx">
+						<text>{{child.name}}</text>
+						<text>{{child.standard ? child.standard : '正常标准'}}</text>
+					</view>
+				</view>
+				<!-- <Info :detail="details"></Info> -->
+			</swiper-item>
+			<swiper-item class="item-wrap">
+				<view class="item-wrap">
+					<swiper class="swiper" v-if="details.files.length > 0" :style="{height: '100vh'}">
+						<swiper-item v-for="(item,index) in details.files" :style="{textAlign: 'center'}">
+							<image :src="item" class="item-img" mode="widthFix"></image>
+						</swiper-item>	
+					</swiper>
+					<!-- <image v-for="(item,index) in details.files" :src="item" class="item-img" mode="widthFix"></image> -->
+					<view class="main-text" style="text-align: center;" v-if="details.files.length == 0">暂无内容</view>
+				</view>
+				<!-- <Info :detail="details"></Info> -->
+			</swiper-item>
+		</swiper>
+	</view>
+</template>
+
+<script>
+	import Info from './info/info.vue'
+	import tabs from '../../components/yc_tabs/yc_tabs.vue'
+	import {
+		Request
+	} from '../../utils/http.js'
+	export default {
+		data() {
+			return {
+				details: {},
+				current: 0,
+				tabList: [{
+					title: '需求物资清单',
+					hasRed: false,
+					num: '99+'
+				}, {
+					title: '防护物资标准',
+					hasRed: false,
+					num: 99
+				}, {
+					title: '图片信息',
+					hasRed: false,
+					num: 99
+				}]
+			}
+		},
+		components: {
+			tabs,
+			Info
+		},
+		methods: {
+			tabClick(e) {
+				let current = typeof e === 'object' ? e.detail.current : e;
+				this.current = current;
+				this.$refs.tab.tabToIndex(current);
+			},
+			loadDetail(id) {
+				let that = this;
+				Request.doInvoke('demand/' + id, 'GET')
+					.then(res => {
+						if (res.code === '10000') {
+							that.details = res.data
+						}
+					}).catch(err => {
+						console.log(err)
+					})
+			}
+		},
+		onLoad(option) {
+			this.loadDetail(option.id)
+		}
+	}
+</script>
+
+<style lang="less" scoped>
+	.item-wrap {
+		box-sizing: border-box;
+		background-color: #FFFFFF;
+		padding: 20upx 20upx 0;
+
+		.item-wuzi {
+			display: flex;
+			align-items: center;
+
+			border-bottom: 1upx solid #f2f2f2;
+			font-family: PingFangSC-Semibold;
+			font-size: 14px;
+			color: #333333;
+			height: 72upx;
+
+			&:last-child {
+				border: none;
+			}
+		}
+	}
+
+	.item-img {
+		width: 80%;
+		margin: 0 auto;
+	}
+</style>
---
+++ pages/details/info/info.vue
@@ -0,0 +1,87 @@
+<template>
+	<view class="info">
+		<view class="info-title">
+			<text class="title-item">医院信息</text>
+		</view>
+		<view class="info-content">
+			<text class="main-text">{{detail.company}}</text>
+			<text class="main-text">地址：{{detail.province + detail.city + detail.area + detail.street}}</text>
+			<view class="flex-between">
+				<text class="main-text">联系人：{{detail.contacts[0].name ? detail.contacts[0].name : ''}}</text>
+				<text class="call-btn" @tap="callSomeOne(detail.contacts[0].phone)">联系电话</text>
+			</view>
+		</view>
+		<view class="info-from flex-between" @tap="toOut(detail.sourceUrl)">
+			<text>来源：官方微博</text>
+			<text> > </text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			detail: {
+				type: Object
+			}
+		},
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+			callSomeOne(phone) {
+				uni.makePhoneCall({
+					phoneNumber: phone //仅为示例
+				});
+			},
+			toOut(url) {
+				window.location.replace(url);
+			}
+		}
+	}
+</script>
+
+<style lang="less" scoped>
+	.info {
+		margin-top: 20upx;
+		padding: 0 20upx;
+		background-color: #FFFFFF;
+
+		.info-title {
+			height: 80upx;
+			border-bottom: 1upx solid #ededed;
+
+			.title-item {
+				line-height: 80upx;
+				padding: 16upx 0;
+				font-size: 32upx;
+				border-bottom: 4upx solid var(--mainColor);
+			}
+		}
+
+		.info-content {
+			display: flex;
+			flex-direction: column;
+			padding: 30upx 0;
+
+			.main-text {
+				font-family: PingFangSC-Semibold;
+				font-size: 28upx;
+				color: #333333;
+				line-height: 50upx;
+			}
+		}
+
+		.info-from {
+			display: flex;
+			align-items: center;
+			height: 120upx;
+			border-top: 1upx solid #ededed;
+			font-family: PingFangSC-Semibold;
+			font-size: 12px;
+			color: #999999;
+		}
+	}
+</style>
